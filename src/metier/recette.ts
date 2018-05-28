129ad44eed9493697e2b956ff9b03d7ea6811d7e
Colin antoine <antoine35.colin@gmail.com>
2018-05-28 18:21:24 +0200

    kasi rien

---
+++ src/metier/recette.ts
@@ -0,0 +1,14 @@
+export class Recette{
+    
+    //keywords : Keyword[];
+    nom : string;
+    image : string;
+    presentation : string;
+    difficulte : number;    //entier sur 5 ou 10
+    nbPers : number;    
+    duree : number;     //nb de min pour la durée
+    tpCuisson : number; //nb de min pour la cuisson
+    //etapes : Etape[];
+    //ingredient : Ingredient[];
+
+}
