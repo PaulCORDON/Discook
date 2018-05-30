91b756ddfa6547d823fbc595b5477759086b313b
Florentin <florentin.paillier@gmail.com>
2018-05-30 18:06:15 +0200

    ca marche

--- src/metier/recette.ts
+++ src/metier/recette.ts
@@ -18,19 +18,8 @@ export class Recette{
     ingredients : Ingredient[] = [];
     reference : firebase.database.Reference;
     
-    constructor(keywords : string[], nom : string, presentation : string, image : string, difficulte : number, duree : number, tpCuisson : number, nbPers : number, etapes : Etape[], ingredients : Ingredient[]){
-        this.keywords = keywords;
-        this.nom = nom;
-        this.presentation = presentation;
-        this.image = image;
-        this.difficulte = difficulte;
-        this.nbPers = nbPers;
-        this.duree = duree;
-        this.tpCuisson = tpCuisson;
-        this.etapes = etapes;
-        this.ingredients = ingredients;
-
-        console.log("/////Création recette : " + nom + ", " + presentation + " pour " + nbPers + " personnes\n    difficulte : " + difficulte + "\n     duree de préparation : " + duree + "\ntemps de cuisson : " + tpCuisson);
+    constructor(){
+        
     }
 
    creerRecette(){
